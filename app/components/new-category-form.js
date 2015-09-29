b3e9a5fffcebb07ec0f7413bcf3bfe2f5439d5a7
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-09-29 15:04:10 -0700

    Add all CL categories.

--- app/components/new-category-form.js
+++ app/components/new-category-form.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  newCategoryForm: false,
+  newCategoryForm: true,
 
   actions: {
-    showNewCategoryForm() {
-      this.set('newCategoryForm', true);
-    },
+    // showNewCategoryForm() {
+    //   this.set('newCategoryForm', true);
+    // },
     saveCategory() {
       var params = {
         name: this.get('name'),
         parentCategory: this.get('parentCategory').toLowerCase(),
       };
-      this.set('newCategoryForm', false);
+      // this.set('newCategoryForm', false);
       this.sendAction('saveCategory', params);
     }
   }
