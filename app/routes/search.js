290bc6a62c32ccdbd98fed31d482c4c9cade183f
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-09-29 16:11:46 -0700

    edit for readability

--- app/routes/search.js
+++ app/routes/search.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(query) {
     return this.store.findAll('post').then(function(posts) {
-      return posts.filterBy('content', query.search_id)
+      return posts.filterBy('title', query.search_id)
     });
   }
 
