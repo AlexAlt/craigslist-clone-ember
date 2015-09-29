0388b5b722a44acd9aee160f802173c932f9eee9
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-09-29 11:58:35 -0700

    Add computed property to alphabetize categories

--- app/components/home-page.js
+++ app/components/home-page.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  sortedCategoriesProps: ['name:asc'],
+  sortedCommunityCategories: Ember.computed.sort('categories.communityCategories', 'sortedCategoriesProps'),
+  sortedPersonalsCategories: Ember.computed.sort('categories.personalsCategories', 'sortedCategoriesProps'),
+  sortedDiscussionCategories: Ember.computed.sort('categories.discussionCategories', 'sortedCategoriesProps'),
+  sortedHousingCategories: Ember.computed.sort('categories.housingCategories', 'sortedCategoriesProps'),
+  sortedForSaleCategories: Ember.computed.sort('categories.forsaleCategories', 'sortedCategoriesProps'),
+  sortedServicesCategories: Ember.computed.sort('categories.servicesCategories', 'sortedCategoriesProps'),
+  sortedJobsCategories: Ember.computed.sort('categories.jobsCategories', 'sortedCategoriesProps'),
+  sortedGigsCategories: Ember.computed.sort('categories.gigsCategories', 'sortedCategoriesProps')
 
 });
---
+++ app/helpers/category-sort.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export function categorySort(params) {
+  
+
+}
+
+export default Ember.Helper.helper(categorySort);
